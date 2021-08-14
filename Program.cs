de5a39fbadfb135d8484a6533f57bb23cc9b4b54
Lucas Batista <lucasbatista12312@gmail.com>
2021-08-14 16:06:41 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoSoftware
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 20;
+            int c = 10;
+            int d = 10;
+
+            Calculadora calc = new Calculadora();
+            int r = calc.Soma(a, b);
+            int r2 = calc.Soma(c, d);
+            Console.WriteLine("A soma do número {0} mais o número {1} é igual a {2}", a, b, r);
+            Console.WriteLine("A soma do número {0} mais o número {1} é igual a {2}", c, d, r2);
+            Console.ReadLine();
+        }
+
+       
+    }
+}
